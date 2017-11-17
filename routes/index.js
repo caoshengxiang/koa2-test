7882b3e96abb2a1a758351176c7dec95a2b88c76
caoshengxiang <1104729815@qq.com>
2017-11-17 18:52:55 +0800

    公司项目测试

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
 
-router.get('/oa', async (ctx, next) => {
+router.get('/', async (ctx, next) => {
   await ctx.render('index', {
     title: 'Hello Koa 2!'
   })
