0fbb865178a9ce764c12ed70929380f833c77589
caoxiangxiang <1104729815@qq.com>
2019-10-29 12:01:12 +0800

    修改邮箱配置

--- config/email_config.js
+++ config/email_config.js
@@ -3,7 +3,9 @@ module.exports = {
   port: 465,
   secure: true, // use SSL
   auth: {
-    user: '1104729815@qq.com',
-    pass: 'xlpiizbmdissjbib'
+    // user: '1104729815@qq.com',
+    // pass: 'xlpiizbmdissjbib',
+    user: '1134767313@qq.com',
+    pass: 'quqndrsmziqoiacb',
   }
 }
