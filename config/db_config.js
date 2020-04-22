8ad607eb8024de2dab4b01f2098afc20577844fc
caoxiangxiang <1104729815@qq.com>
2020-04-22 15:11:39 +0800

    数据库外网关闭

--- config/db_config.js
+++ config/db_config.js
@@ -2,8 +2,9 @@ module.exports = {
     // dbLocal: 'mongodb://localhost:27017/',
     // dbLocal: 'mongodb://106.12.193.173:27017/', // 个人服务器生产库
     // dbLocal: 'mongodb://49.235.131.71:27017/', // 71 生产库 国内
+    //  dbLocal: '52.14.39.214:27017/', // 生产库 国外（外网关闭）
     name: 'root',
     pwd: '20200421Cosplay!!!',
-    dbLocal: '52.14.39.214:27017/', // 生产库 国外
+    dbLocal: 'localhost:27017/', // 生产库 国外
     dbName: 'cosplay',
 }
